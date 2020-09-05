eec3bf42a8c0748ed55870c738a0769ba4bec53c
grmxd <giacomo.ratta@diymaxeda.com>
2020-09-05 10:27:57 +0200

    validation with redux-form

--- 270-twitch-app/client/src/components/streams/StreamCreate.js
+++ 270-twitch-app/client/src/components/streams/StreamCreate.js
@@ -7,13 +7,30 @@ import { Field, reduxForm } from 'redux-form';
 class StreamCreate extends React.Component {
   // this.props will have a lot of props!
 
-  renderInput({ input, label }) {
+  renderError({ error, touched}) {
+    if (touched && error) {
+      // ui error message has display:none by default; see <form>
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  // turned renderInput into arrow function because of an error related to 'this' and context :/
+  renderInput = ({ input, label, meta }) => {
     // renderInput(formProps) --> formProps = { input:{...}, meta:{...} }
     // we do not need to specify each prop!
+
+    // (alternative): className="field error" will show red fields
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    
     return (
-      <div className="field">
+      <div className={className}>
         <label>{ label }</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
   }
@@ -33,7 +50,7 @@ class StreamCreate extends React.Component {
       //    - with redux-form we must use handleSubmit
       //    - no need of event.preventDefault() in onSubmit()
 
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
         <button className="ui button primary">Submit</button>
@@ -42,6 +59,20 @@ class StreamCreate extends React.Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  // it will be passed to each component (see homonym prop.) rendered as Field, in 'meta' obj
+  return errors;
+}
+
 export default reduxForm({
-  form: 'streamCreate' // name for this specific form
+  form: 'streamCreate', // name for this specific form
+  validate              // form validation with error messages
 })(StreamCreate);
